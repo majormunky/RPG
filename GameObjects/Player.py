cab1734e56fb88e80ea55eb49e3a3ef6df4330e2
Josh Bright <josh.bright@gmail.com>
2020-05-26 21:36:09 -0700

    Scrolling around the world with the camera works now

--- GameObjects/Player.py
+++ GameObjects/Player.py
@@ -8,6 +8,12 @@ class Player:
 		self.x = 0
 		self.y = 0
 		self.color = (0, 0, 200)
+		self.directions = {
+			pygame.K_DOWN: "down",
+			pygame.K_UP: "up",
+			pygame.K_RIGHT: "right",
+			pygame.K_LEFT: "left"
+		}
 
 	def update(self, dt):
 		pass
@@ -19,17 +25,22 @@ class Player:
 		return pygame.Rect(self.x, self.y, self.width, self.height)
 
 	def handle_event(self, event, player_check):
+		direction = None
 		if event.type == pygame.KEYUP:
 			new_rect = self.get_rect()
 			if event.key == pygame.K_UP:
 				new_rect.y -= self.height
+				direction = self.directions[event.key]
 			elif event.key == pygame.K_DOWN:
 				new_rect.y += self.height
+				direction = self.directions[event.key]
 			elif event.key == pygame.K_LEFT:
 				new_rect.x -= self.width
+				direction = self.directions[event.key]
 			elif event.key == pygame.K_RIGHT:
 				new_rect.x += self.width
+				direction = self.directions[event.key]
 			
-			if player_check(new_rect):
+			if player_check(new_rect, direction):
 				self.x = new_rect.x
 				self.y = new_rect.y
--- main.py
+++ main.py
@@ -25,7 +25,8 @@ class Game:
 		self.world.draw(canvas, camera_rect)
 		self.player.draw(canvas, camera_rect)
 
-	def check_player_position(self, rect):
+	def check_player_position(self, rect, direction):
+		print("checking player position")
 		fixed_rect = pygame.Rect(
 			rect.x + self.camera.x,
 			rect.y + self.camera.y,
@@ -33,17 +34,56 @@ class Game:
 			rect.height
 		)
 
-		if not self.screenrect.contains(fixed_rect):
+		if not self.screenrect.contains(rect):
+			print("Player is going to be off of the screen, returning false")
 			return False
 
 		if fixed_rect.right > self.world.get_width() or fixed_rect.bottom > self.world.get_height():
+			print("Player is going to be outside of the world, returning false")
 			return False
 
 		tile_info = self.world.get_tile_at_rect(fixed_rect)
 		if tile_info["solid"]:
+			print("Player tried to walk on a solid tile, returning false")
 			return False
 
-		return True
+		map_rect = self.world.get_rect()
+		camera_rect = self.camera.get_rect()
+		has_moved = False
+		if map_rect.contains(rect):
+			if direction == "right":
+				if rect.right > self.screenrect.width - self.map_pad:
+					# player is in an area where we should scroll the camera
+					camera_moved = self.camera.move(camera_rect, map_rect, 32, direction)
+					if not camera_moved:
+						has_moved = True
+				else:
+					has_moved = True
+			if direction == "left":
+				if rect.x < self.map_pad:
+					# player is in an area where we should scroll the camera
+					camera_moved = self.camera.move(camera_rect, map_rect, 32, direction)
+					if not camera_moved:
+						has_moved = True
+				else:
+					has_moved = True
+			if direction == "up":
+				if rect.y < self.map_pad:
+					# player is in an area where we should scroll the camera
+					camera_moved = self.camera.move(camera_rect, map_rect, 32, direction)
+					if not camera_moved:
+						has_moved = True
+				else:
+					has_moved = True
+			if direction == "down":
+				if rect.bottom > self.screenrect.bottom - self.map_pad:
+					# player is in an area where we should scroll the camera
+					camera_moved = self.camera.move(camera_rect, map_rect, 32, direction)
+					if not camera_moved:
+						has_moved = True
+				else:
+					has_moved = True
+		return has_moved
 
 	def handle_event(self, event):
 		self.player.handle_event(event, self.check_player_position)
