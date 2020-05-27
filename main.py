63f81c3fe4e6743d5e2d0cd72553f01ae315c93d
Josh Bright <josh.bright@gmail.com>
2020-05-26 20:34:23 -0700

    Setup a basic world class

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
 import pygame
 from Engine.Engine import Engine
+from Engine.Grid import Grid
+
+
+class World:
+	def __init__(self):
+		self.tile_width = 32
+		self.tile_height = 32
+		self.grid = Grid([
+			["0", "0", "0", "1", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+			["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+		])
+		self.tile_info = {
+			"0": {
+				"color": (0, 200, 0),
+				"solid": False
+			},
+			"1": {
+				"color": (100, 100, 100),
+				"solid": True
+			}
+		}
+		self.image = None
+		self.render_map()
+
+	def render_map(self):
+		self.image = pygame.Surface((
+			self.tile_width * self.grid.width,
+			self.tile_height * self.grid.height,
+		), pygame.SRCALPHA)
+		for y in range(self.grid.height):
+			for x in range(self.grid.width):
+				cell_type = self.grid.get_cell(x, y)
+				cell_info = self.tile_info.get(cell_type, None)
+				if cell_info:
+					pygame.draw.rect(self.image, cell_info["color"], (x * self.tile_width, y * self.tile_height, self.tile_width, self.tile_height))
+
+
+	def update(self, dt):
+		pass
+
+	def draw(self, canvas):
+		canvas.blit(self.image, (0, 0))
+
+	def handle_event(self, event):
+		pass
 
 
 class Player:
@@ -21,12 +74,14 @@ class Player:
 
 class Game:
 	def __init__(self):
+		self.world = World()
 		self.player = Player()
 
 	def update(self, dt):
 		pass
 
 	def draw(self, canvas):
+		self.world.draw(canvas)
 		self.player.draw(canvas)
 
 	def handle_event(self, event):
