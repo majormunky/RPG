4f6a9021a43583149a3d8d3dd4f8b3aa89d24c35
Josh Bright <josh.bright@gmail.com>
2020-05-26 20:24:21 -0700

    Added some basic classes for the game and the player

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import pygame
+from Engine.Engine import Engine
+
+
+class Player:
+	def __init__(self):
+		self.width = 32
+		self.height = 32
+		self.x = 0
+		self.y = 0
+
+	def update(self, dt):
+		pass
+
+	def draw(self, canvas):
+		pygame.draw.rect(canvas, (100, 100, 100), (self.x, self.y, self.width, self.height))
+
+	def handle_event(self, event):
+		pass
+
+
+class Game:
+	def __init__(self):
+		self.player = Player()
+
+	def update(self, dt):
+		pass
+
+	def draw(self, canvas):
+		self.player.draw(canvas)
+
+	def handle_event(self, event):
+		pass
+
+
 def main():
-	print('Run')
+	e = Engine(Game)
+	e.game_loop()
+
 
 if __name__ == '__main__':
 	main()
