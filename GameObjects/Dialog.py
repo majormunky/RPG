cfbf264c41cdc0afd6c8a4fa89df001a8199af24
Josh Bright <josh.bright@gmail.com>
2020-05-27 22:10:09 -0700

    Updated the dialog class to be able to display text

--- GameObjects/Dialog.py
+++ GameObjects/Dialog.py
@@ -1,5 +1,6 @@
 import pygame
 from Engine.Config import get_screenrect
+from Engine.Text import text_surface
 
 
 class Dialog:
@@ -14,12 +15,25 @@ class Dialog:
 		)
 		self.image = None
 		self.active = False
-		self.render()
+		self.lines = None
+		self.render_lines(["test", "test2"])
 
 	def render(self):
 		self.image = pygame.Surface((self.screenrect.width, self.screenrect.height), pygame.SRCALPHA)
 		self.image.fill((100, 100, 100, 128))
 		pygame.draw.rect(self.image, (0, 0, 200), self.dialog_rect)
+
+		x = self.dialog_rect.x + self.padding
+		y = self.dialog_rect.y + self.padding
+
+		for line in self.lines:
+			text = text_surface(line, font_size=32)
+			self.image.blit(text, (x, y))
+			y += text.get_rect().height + self.padding
+
+	def render_lines(self, lines):
+		self.lines = lines
+		self.render()
 
 	def update(self, dt):
 		pass
